c1babf46d82287c9ba5f859bf25cf6fbede98047
mstoews <mstoews@hotmail.com>
2026-06-13 21:16:56 +0900

    Initial Commit

---
+++ GlutenFree/GlutenFreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlutenFreeApp.swift
+//  GlutenFree
+//
+//  Created by Murray Toews on 6/13/26.
+//
+
+import SwiftUI
+
+@main
+struct GlutenFreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GlutenFreeTests/GlutenFreeTests.swift
@@ -0,0 +1,19 @@
+//
+//  GlutenFreeTests.swift
+//  GlutenFreeTests
+//
+//  Created by Murray Toews on 6/13/26.
+//
+
+import Testing
+@testable import GlutenFree
+
+struct GlutenFreeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
